ba026abc4d814ffc3561fab647fd4dbfd116d3d1
Alonso Aponte <alonpoh92@hotmail.com>
2022-11-15 16:12:58 -0500

    Avance segunda entrega proyecto final

---
+++ DB/db.config.js
@@ -0,0 +1,19 @@
+const firebaseConfig = require('./firebase/firebase.config.json');
+const envConfig = require('../config');
+
+module.exports = {
+    memory: {
+        products: 'products',
+        carts: 'carts'
+    },
+    file: {
+        products: 'data/products.json',
+        carts: 'data/carts.json'
+    },
+    mongodb: {
+        uri: `mongodb+srv://${envConfig.DB_USER}:${envConfig.DB_PASSWORD}@coderhousebackend.mcdak1d.mongodb.net/?retryWrites=true&w=majority`
+    },
+    firebase: {
+        credentials: firebaseConfig
+    }
+}
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DATASOURCE: process.env.DATASOURCE
+}
