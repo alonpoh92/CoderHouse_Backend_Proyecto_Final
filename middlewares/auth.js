61a096d39770291b591a5c4d7855438f7e01c843
Alonso Aponte <alonpoh92@hotmail.com>
2023-02-22 01:36:17 -0500

    Tercera entrega proyecto final

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,19 @@
-const auth = (failRedirect='/') => {
+const auth = (userlogged, failRedirect='/') => {
   return async (req, res, next) => {
-    if (req.isAuthenticated()) {
-      next();
-    }
-    else {
-      res.redirect(failRedirect);
+    if(userlogged){
+      if (req.isAuthenticated()) {
+        next();
+      }
+      else {
+        res.redirect(failRedirect);
+      }
+    }else{
+      if (!req.isAuthenticated()) {
+        next();
+      }
+      else {
+        res.redirect(failRedirect);
+      }
     }
   };
 }
--- routers/app.routes.js
+++ routers/app.routes.js
@@ -8,29 +8,29 @@ const router = express.Router();
 
 router.use('/api', apiRoutes);
 
-router.get('/', authMiddleware('/login'), async (req, res) => {
+router.get('/', authMiddleware(true, '/login'), async (req, res) => {
   const data = req.user;
   data.qtyItems = 2;
   data.hasItems = data.qtyItems > 0;
   return res.render('test', data);
 });
 
-router.get('/login', (req, res) => {
+router.get('/login', authMiddleware(false, '/'), (req, res) => {
   return res.sendFile(path.resolve(__dirname, '../public/login.html'));
 });
 
-router.get('/profile', (req, res) => {
+router.get('/profile', authMiddleware(true, '/login'), (req, res) => {
   const data = req.user;
   data.qtyItems = 0;
   data.hasItems = data.qtyItems > 0;
   return res.render('profile', data);
 });
 
-router.get('/signin-error', (req, res) => {
+router.get('/signin-error', authMiddleware(false, '/'), (req, res) => {
   return res.render('signin-error', {layout: false});
 });
 
-router.get('/signup-error', (req, res) => {
+router.get('/signup-error', authMiddleware(false, '/'), (req, res) => {
   return res.render('signup-error', {layout: false});
 });
 
